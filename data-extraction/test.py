ad4d361aae376a78bd845cc7c397da818a3e163b
sarangg06 <sarangdeshpande.ssd@gmail.com>
2026-06-11 23:42:41 +0530

    yt-comment-scraper-core-v1

--- data-extraction/test.py
+++ data-extraction/test.py
@@ -1,1 +1,12 @@
-print("hello world")
+from youtube_comment_downloader import YoutubeCommentDownloader
+
+downloader = YoutubeCommentDownloader()
+
+comments = downloader.get_comments_from_url(
+    "https://www.youtube.com/watch?v=HSN7ISzmDfY"
+)
+
+for i, comment in enumerate(comments):
+    print(comment["text"])
+    if i == 99:
+        break
