06f2a7f6a181ecc544bbd3615a48dab64dc115f4
sarangg06 <sarangdeshpande.ssd@gmail.com>
2026-06-16 00:57:02 +0530

    core function addition w. updated code

--- data-extraction/extraction_groups/neutral.py
+++ data-extraction/extraction_groups/neutral.py
@@ -1,2 +1,34 @@
-def get_neutral_comments():
-    pass
+from utils.video_extraction import get_videos
+from utils.comments_extraction import get_comments
+from utils.save_comments_to_csv import save_comments
+
+QUERIES = [
+    "today was normal",
+    "weather is nice",
+    "good morning",
+    "what are you doing",
+    "random thoughts",
+    "daily routine",
+    "ate lunch",
+    "going to work",
+    "watching TV",
+    "reading a book",
+    "walking outside",
+    "cooking dinner",
+    "shopping today",
+    "weekend plans",
+    "traffic today",
+    "learning python",
+    "new movie",
+    "travel plans",
+    "news update",
+    "general discussion"
+]
+
+def get_neutral_comments_by_videos(youtube):
+
+    videos = get_videos(youtube, QUERIES)
+
+    for video in videos:
+        comments_df = get_comments(youtube, video["video_id"])
+        save_comments(comments_df, "neutral", video["video_id"])
