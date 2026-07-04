786732bdaaf15ac82e3073ac686d8fcba3cf4459
Aryan Khaire <aryan.khaire001@gmail.com>
2026-07-04 22:37:53 +0530

    Completed data-transformation

---
+++ data-transformation/main.py
@@ -0,0 +1,23 @@
+from read_parquet_from_blob import read_parquet_from_blob
+from emotion_generator import generate_emotion_scores
+
+DATASET_NAME = "Anxiety"
+
+RESUME_COMMENT_ID = None
+
+BATCH_SIZE = 1000
+
+
+def main():
+
+    df = read_parquet_from_blob(DATASET_NAME)
+
+    generate_emotion_scores(
+        df=df,
+        comment_id=RESUME_COMMENT_ID,
+        batch_size=BATCH_SIZE
+    )
+
+
+if __name__ == "__main__":
+    main()
